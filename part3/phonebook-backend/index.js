9d7109b31ce8f93a21e6ac0f60e71f573318bbe1
cicada <cicada>
2025-10-13 23:00:21 +0800

    3.8*

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan')
 
 const PORT = 3001
 let persons = [
@@ -26,6 +27,8 @@ let persons = [
 
 const app = express();
 app.use(express.json())
+morgan.token('body', req => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
