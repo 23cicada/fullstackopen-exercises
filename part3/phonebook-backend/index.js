a13d921ad3bfd9aca6e3aa12fe9c448f49d11bbe
cicada <hey.changjun@gmail.com>
2025-10-27 17:34:03 +0800

    3.13.

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,29 +1,9 @@
 const express = require('express');
 const morgan = require('morgan')
+const Person = require('./models/person')
+require('dotenv').config()
 
-const PORT = process.env.PORT || 3001
-let persons = [
-  {
-    "id": "1",
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": "2",
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": "3",
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
+const PORT = process.env.PORT || 3000
 
 const app = express();
 morgan.token('body', req => JSON.stringify(req.body))
@@ -35,23 +15,17 @@ app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => res.json(persons))
 })
 
 app.get('/api/persons/:id', (req, res) => {
   const id = req.params.id
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  Person.findById(id).then(person => res.json(person))
 })
 
 app.delete('/api/persons/:id', (req, res) => {
   const id = req.params.id
-  persons = persons.filter(person => person.id !== id)
-  res.status(204).end()
+  Person.findByIdAndDelete(id).then(() => res.status(204).end())
 })
 
 app.post('/api/persons', (req, res) => {
@@ -61,12 +35,13 @@ app.post('/api/persons', (req, res) => {
   } else if (persons.find(person => person.name === name)) {
     return res.status(400).json({ error: 'name must be unique' })
   }
-  const created= { name, number, id: Math.random().toString(36).substring(2) }
-  persons = [...persons, created]
-  res.json(created)
+  const created= { name, number }
+  const person = new Person(created)
+  person.save().then(saved => res.json(saved))
 })
 
 app.get('/info', (req, res) => {
+  const persons = Person.find({})
   res.send(`
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${new Date()}</p>
