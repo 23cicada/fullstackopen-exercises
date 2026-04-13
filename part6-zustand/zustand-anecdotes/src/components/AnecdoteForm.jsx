1bfa6084ed331765d52d7a6ed24b6bd45ca272f8
cicada <cicada>
2026-04-13 22:11:39 +0800

    zustand: 6.1 - 6.6

---
+++ part6-zustand/zustand-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,26 @@
+import { useAnecdoteActions } from '../store'
+
+const AnecdoteForm = () => {
+  const { create } = useAnecdoteActions()
+
+  const handleCreate = async event => {
+    event.preventDefault()
+    const content = event.target.content.value
+    event.target.content.value = ''
+    create(content)
+  }
+
+  return (
+    <>
+      <h2>create new</h2>
+      <form onSubmit={handleCreate}>
+        <div>
+          <input name="content" />
+        </div>
+        <button>create</button>
+      </form>
+    </>
+  )
+}
+
+export default AnecdoteForm
