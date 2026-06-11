807179b53ee754346f691f114bba50300ebc3c2b
cicada <hey.changjun@gmail.com>
2026-06-11 16:47:45 +0800

    test health check

--- part11/fs-pokedex-main/app.js
+++ part11/fs-pokedex-main/app.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
-app.get('health', () => {
+app.get('/health', () => {
   throw new Error('test error')
   // res.send('ok')
 })
